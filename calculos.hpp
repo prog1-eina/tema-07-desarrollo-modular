8b4822904d56a39df5700a9b0028dd43931354cd
Miguel Ángel Latre Abadía <latre@unizar.es>
2020-10-18 10:45:43 +0200

    cambio la extensión «.h» a «.hpp»

--- calculos.hpp
+++ calculos.hpp
@@ -1,18 +1,18 @@
-﻿/*********************************************************************************
+﻿/*******************************************************************************
  * Curso de Programación 1. Tema 7 y práctica 3
  * Autores: Miguel Ángel Latre y Javier Martínez
- * Última revisión: 17 de octubre de 2018
- * Resumen: Fichero calculos.h de interfaz del módulo de biblioteca
+ * Última revisión: 18 de octubre de 2020
+ * Resumen: Fichero calculos.hpp de interfaz del módulo de biblioteca
  *          «calculos» que facilita algunas funciones para trabajar con
  *          datos de tipo entero
- ********************************************************************************/
+ ******************************************************************************/
 
 /*
  * Pre:  ---
  * Post: Ha devuelto el número de cifras de «n» cuando «n» se escribe en
  *       base 10.
  */
-int numCifras(int n);
+unsigned int numCifras(int n);
 
 
 /*
@@ -20,7 +20,7 @@ int numCifras(int n);
  * Post: Ha devuelto la suma de las cifras de «n» cuando «n» se escribe en
  *       base 10.
  */
-int sumaCifras(int n);
+unsigned int sumaCifras(int n);
 
 
 /*
@@ -28,7 +28,7 @@ int sumaCifras(int n);
  *  Post: Ha devuelto la «i»-ésima cifra menos significativa de «n» cuando
  *        «n» se escribe en base 10.
  */
-int cifra(int n, int i);
+unsigned int cifra(int n, unsigned int i);
 
 
 /*
@@ -40,21 +40,21 @@ int imagen(int n);
 
 
 /*
- *  Pre:  n ≥ 0
+ *  Pre:  ---
  *  Post: Ha devuelto el factorial de «n».
  */
-int factorial(int n);
+unsigned int factorial(unsigned int n);
 
 
 /*
  *  Pre:  ---
  *  Post: Ha devuelto true si y solo si «n» es un número primo.
  */
-bool esPrimo (int n);
+bool esPrimo(unsigned int n);
 
 
 /*
- *  Pre: a != 0 o b != 0
- *  Post: Ha devuelto el máximo común divisor de <a> y <b>.
+ *  Pre: a ≠ 0 o b ≠ 0
+ *  Post: Ha devuelto el máximo común divisor de «a» y «b».
  */
-int mcd (int a, int b);
+unsigned int mcd(int a, int b);
